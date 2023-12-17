79c5c5c6af88a9b547b19518ce6bd617da78ad5d
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2023-12-16 20:11:45 -0500

    can override weights

--- src/weight/weight.service.ts
+++ src/weight/weight.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { Entry } from "src/schemas/entry.schema";
 import { Weight } from "src/schemas/weight.schema";
 
 @Injectable()
 export class WeightService {
-  constructor(@InjectModel(Weight.name) private weightModel: Model<Entry>) {}
+  constructor(@InjectModel(Weight.name) private weightModel: Model<Weight>) {}
 
   public async getWeight() {
     const entries = await this.weightModel.find();
@@ -15,13 +14,9 @@ export class WeightService {
 
   public async addWeight(date: string, weight: string) {
     try {
-      const weightToday = await this.weightModel.findOne({ date });
-
-      if (weightToday) return { status: 409, message: "Already added weight for today!" };
-
       const weightRes = await this.weightModel.create({ date, weight });
 
-      return { status: 200, data: { weightRes }, message: "Weight successfully created" };
+      return { status: 200, data: { weight: weightRes.weight, date: weightRes.date }, message: "Weight successfully created" };
     } catch (err) {
       return { status: err.status, message: err.message || "Was not able to create entry" };
     }
