dce0e29c6986b56b269b7c4c2571b248757f1ed3
Cruz Cordero <cruzcordero7600@gmail.com>
2024-03-02 22:51:30 -0500

    removing other setups

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
-import { CalorieModule } from "./calories/calorie.module";
 
 @Module({
   imports: [
-    CalorieModule,
     ConfigModule.forRoot({
       envFilePath: ".env",
       isGlobal: true,
